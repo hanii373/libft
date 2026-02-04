75f1815b01f8c02317044405c0d4315ec7abaeca
Gohar Galstyan <gogalsty@c2r6s1.42yerevan.am>
2026-02-04 17:37:58 +0400

    corrections

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: gogalsty <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 18:04:18 by gogalsty          #+#    #+#             */
-/*   Updated: 2026/02/02 18:42:33 by gogalsty         ###   ########.fr       */
+/*   Updated: 2026/02/04 17:29:53 by gogalsty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strdup(char *str)
 
 	i = 0;
 	len = ft_strlen(str);
-	dup = (char *)malloc((len + 1) * sizeof(char));
+	dup = (char *)malloc(len + 1);
 	while (str[i])
 	{
 		dup[i] = str[i];
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gogalsty <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 14:50:14 by gogalsty          #+#    #+#             */
-/*   Updated: 2026/02/02 17:42:14 by gogalsty         ###   ########.fr       */
+/*   Updated: 2026/02/04 16:30:04 by gogalsty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,17 @@ void			*ft_memset(void *s, int c, size_t n);
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
 size_t			ft_strlen(const char *str);
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
-int				ft_lower(char *str);
-int				ft_upper(char *str);
+int				ft_tolower(char *str);
+int				ft_toupper(char *str);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+int				ft_atoi(char *str);
+void			*ft_calloc(size_t nmemb, size_t size);
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strdup(char *str);
+char			*ft_strnstr(const char *big, const char *little, size_t len);
+int				ft_strncmp(char *s1, char *s2, unsigned int n);
+char			*ft_strrchr(const char *s, int c);
+
 #endif
